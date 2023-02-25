f89bfeceabdb8c2476e1a2a57140e1247696c462
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2023-02-25 11:40:29 -0300

    Creating store user methods

--- src/modules/core/users/users.service.ts
+++ src/modules/core/users/users.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const registredUser = await this.prisma.user.findFirst({
+      where: {
+        email: {
+          equals: data.email,
+        },
+      },
+    });
+
+    if (registredUser) {
+      throw new BadRequestException(
+        'There is already a registered user with this email',
+      );
+    }
+
+    const createdUser = await this.prisma.user.create({
+      data,
+    });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
   }
 
   // findAll() {
